f6beb3d12c41f21a6d111cf84e449ba36fd85c97
Vladimir <sobranie.ua@gmail.com>
2017-07-24 20:39:29 +0300

    companies page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,48 @@
 $(function() {
 	signUp();
+	loadCompanies();
+	loadNews();
 });
+
+function loadNews() {
+	$.ajax({
+			method: "GET",
+			url: "http://codeit.pro/frontTestTask/news/getList"
+		})
+		.done(function(data) {
+			$('.news').removeClass('loading');
+			for (var i = 0; i < data.list.length; i++) {
+				$('.newsList').append('<div class="item clear"><div class="clear"><img src="' + data.list[i].img + '" /><a class="header" href="' + data.list[i].link + '">' + data.list[i].description.substring(0,10) + '</a><div class="text">' + (data.list[i].description.length > 100 ? data.list[i].description.substring(0,100) + '...' : data.list[i].description) + '</div></div><div><strong><em>Author: </em></strong>' + data.list[i].author + '</div><div><strong><em>Public: </em></strong>' + data.list[i].date + '</div></div>')
+				$('.pager').append('<li></li>')
+			}
+			$('.item').eq(0).addClass('active');
+			$('.pager li').eq(0).addClass('active');
+			
+			$('.pager li').on('click', function() {
+				var index = $('.pager li').index($(this));
+				$('.item').removeClass('active');
+				$('.pager li').removeClass('active');
+				$(this).addClass('active');
+				$('.item').eq(index).addClass('active');
+			});
+		});
+	
+}
+
+function loadCompanies() {
+	$.ajax({
+			method: "GET",
+			url: "http://codeit.pro/frontTestTask/company/getList"
+		})
+		.done(function(data) {
+			$('.totalCompanies').removeClass('loading');
+			$('.totalNumber').text(data.list.length);
+			$('.listCompanies').removeClass('loading');
+			for (var i = 0; i < data.list.length; i++) {
+				$('.list').append('<li><a href="#">' + data.list[i].name + '</a></li>')
+			}
+		});
+}
 
 function signUp() {
 	$('#signUpForm').find('.signUpSubmit').on('click', function(event) {
@@ -18,10 +60,10 @@ function signUp() {
 			url: "http://codeit.pro/frontTestTask/user/registration",
 			data: $('#signUpForm').serialize()
 		})
-		.done(function(msg) {
-			showMessage(msg);
+		.done(function(data) {
+			showMessage(data);
 			
-			if (msg.status == 'OK') {
+			if (data.status == 'OK') {
 				location.href = "companies.html";
 			}
 		});
@@ -39,7 +81,6 @@ function showMessage(message){
 }
 
 function validateForm() {
-	var errorClass = 'has-error';
 	var isValid = true;
 	
 	$('.formField').each(function() {
@@ -47,10 +88,10 @@ function validateForm() {
 		var input = field.find('input, select');
 		
 		if (input.val() == '') {
-			field.addClass(errorClass);
+			field.addClass('has-error');
 			isValid = false;
 		} else {
-			field.removeClass(errorClass);
+			field.removeClass('has-error');
 		}
 	});
 	
@@ -59,10 +100,10 @@ function validateForm() {
 		var input = field.find('input');
 		
 		if (!input.is(':checked')) {
-			field.addClass(errorClass);
+			field.addClass('has-error');
 			isValid = false;
 		} else {
-			field.removeClass(errorClass);
+			field.removeClass('has-error');
 		}
 	});
 	
